b055b31bd384ea549cbc4c9feaff23e832bebef4
Hp <oluwafisayomiogunyemi@gmail.com>
2023-01-12 12:09:42 +0100

    Finished with serverside codes

--- lib/auth/firestore_methods.dart
+++ lib/auth/firestore_methods.dart
@@ -61,4 +61,109 @@ class FirestoreMethods {
       }
     }
   }
+
+  // function to post a comment
+  Future<void> postComment(String postId, String text, String uid, String name,
+      String profilePic, List likes) async {
+    try {
+      if (text.isNotEmpty) {
+        String commentId = const Uuid().v1();
+        await _firestore
+            .collection("post")
+            .doc(postId)
+            .collection("comments")
+            .doc(commentId)
+            .set({
+          "profilePic": profilePic,
+          "name": name,
+          "uid": uid,
+          "text": text,
+          "commentId": commentId,
+          "datePublished": DateTime.now(),
+          "likes": []
+        });
+      } else {
+        print("Text is empty");
+      }
+    } catch (e) {
+      print(
+        e.toString(),
+      );
+    }
+  }
+
+  // function to like a comment
+  Future<void> likeComment(
+      String postId, String uid, List likes, String commentId) async {
+    try {
+      if (likes.contains(uid)) {
+        await _firestore
+            .collection("post")
+            .doc(postId)
+            .collection("comments")
+            .doc(commentId)
+            .update({
+          "likes": FieldValue.arrayRemove([uid]),
+        });
+      } else {
+        await _firestore
+            .collection("post")
+            .doc(postId)
+            .collection("comments")
+            .doc(commentId)
+            .update({
+          "likes": FieldValue.arrayUnion([uid]),
+        });
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print(
+          e.toString(),
+        );
+      }
+    }
+  }
+
+  // function to delete a post
+  Future<void> deletePost(String postId) async {
+    try {
+      await _firestore.collection("post").doc(postId).delete();
+    } catch (err) {
+      print(
+        err.toString(),
+      );
+    }
+  }
+
+  // function to follow a user
+  Future<void> followUser(String uid, String followId) async {
+    try {
+      DocumentSnapshot snapshot =
+          await _firestore.collection("users").doc(uid).get();
+
+      List following = (snapshot.data()! as dynamic)["following"];
+
+      // check if user is already following someone
+      if (following.contains(followId)) {
+        await _firestore.collection("users").doc(followId).update({
+          "followers": FieldValue.arrayRemove([uid])
+        });
+
+        await _firestore.collection("users").doc(followId).update({
+          "following": FieldValue.arrayRemove([followId])
+        });
+      } // check if user is not following the other account
+      else {
+        await _firestore.collection("users").doc(followId).update({
+          "followers": FieldValue.arrayUnion([uid])
+        });
+
+        await _firestore.collection("users").doc(followId).update({
+          "following": FieldValue.arrayUnion([followId])
+        });
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
 }
