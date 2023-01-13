ad6b4eb069e4a9539a76052055b853c6f38a0634
Hp <oluwafisayomiogunyemi@gmail.com>
2023-01-13 12:53:49 +0100

    Added a Utilities file

--- lib/variables/colors.dart
+++ lib/variables/colors.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
-const backgroundColor = Color(0xFF29BE18);
-const textColor = Color(0xFFFFFFFF);
-const buttonColor = Color(0xFF77AA4F);
+const backgroundColor = Color.fromRGBO(0, 0, 0, 1);
+const textColor = Colors.white;
+const buttonColor = Color.fromRGBO(0, 149, 246, 1);
 const buttonTextColor = Color(0xFFFFFFFF);
+const secondaryColor = Colors.grey;
 // const iconButtonColor = Color(0xFFFFFFFF);
 // const backgroundColor2 = Color(0xFFFFE5BD);
 // const buttonColor2 = Color(0xFFFBBA59);
