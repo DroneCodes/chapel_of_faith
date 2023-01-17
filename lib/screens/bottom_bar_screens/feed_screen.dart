e481c7983915b42d3414de5341068637144c7117
Hp <oluwafisayomiogunyemi@gmail.com>
2023-01-17 23:42:36 +0100

    Finished feed screen

--- lib/screens/bottom_bar_screens/feed_screen.dart
+++ lib/screens/bottom_bar_screens/feed_screen.dart
@@ -1,3 +1,6 @@
+import 'package:chapel_of_faith/variables/colors.dart';
+import 'package:chapel_of_faith/widgets/post_card.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class FeedScreen extends StatefulWidget {
@@ -10,6 +13,41 @@ class FeedScreen extends StatefulWidget {
 class _FeedScreenState extends State<FeedScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        centerTitle: false,
+        title: Image.asset(
+          "assets/chapel_logo.jpg",
+          color: primaryColor,
+          height: 32,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.message,
+            ),
+          ),
+        ],
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("post").snapshots(),
+        builder: (context,
+            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return ListView.builder(
+            itemBuilder: (context, index) => PostCard(
+              snap: snapshot.data!.docs[index].data(),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
